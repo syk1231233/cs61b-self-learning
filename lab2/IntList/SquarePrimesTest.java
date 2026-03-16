d48091e92596d325f4eb31df7e4bf6de13724331
syk1231233 <1846954776@qq.com>
2026-03-16 09:50:00 +0800

    完成lab2“

---
+++ lab2/IntList/SquarePrimesTest.java
@@ -0,0 +1,36 @@
+package IntList;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SquarePrimesTest {
+
+    /**
+     * Here is a test for isPrime method. Try running it.
+     * It passes, but the starter code implementation of isPrime
+     * is broken. Write your own JUnit Test to try to uncover the bug!
+     */
+    @Test
+    public void testSquarePrimesSimple() {
+        IntList lst = IntList.of(14, 15, 16, 17, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimes_AllEven(){
+        IntList lst = IntList.of(2,4,6,8,10);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 4 -> 6 -> 8 -> 10", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimes_AllOdd(){
+        IntList lst = IntList.of(1,3,5,7,9);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 9 -> 25 -> 49 -> 9", lst.toString());
+        assertTrue(changed);
+    }
+}
