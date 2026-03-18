146586108a055f5825f0ca9e042b4a41283b496d
syk1231233 <1846954776@qq.com>
2026-03-18 15:26:24 +0800

    fix lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,17 +1,16 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Syk
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * Return Collatz Sequence's next number.
+     * @param n the pre number.
+     * @return the parameter's next number.
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+        if(n % 2 == 0) return n / 2;
+        else return n * 3 + 1;
     }
 
     public static void main(String[] args) {
