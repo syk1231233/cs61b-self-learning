e82391fd698ac279677b28bc9b36f167b22ed40c
syk1231233 <1846954776@qq.com>
2026-03-19 11:23:31 +0800

    完成lab6

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -1,6 +1,7 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import static capers.Utils.*;
 
 /** A repository for Capers 
@@ -18,9 +19,8 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
+    static final File CAPERS_FOLDER = join(CWD,".capers"); // TODO Hint: look at the `join`
                                             //      function in Utils
-
     /**
      * Does required filesystem operations to allow for persistence.
      * (creates any necessary folders or files)
@@ -31,7 +31,8 @@ public class CapersRepository {
      *    - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        CAPERS_FOLDER.mkdirs();
+        Dog.DOG_FOLDER.mkdirs();
     }
 
     /**
@@ -40,7 +41,18 @@ public class CapersRepository {
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        File storyFile = Utils.join(CAPERS_FOLDER,"story.txt");
+        if(!storyFile.exists()){
+            try {
+                storyFile.createNewFile();
+            } catch (IOException e){
+                Utils.exitWithError("Create file story failure.");
+            }
+        }
+        String oldContent = readContentsAsString(storyFile);
+        if(oldContent != null && !oldContent.isEmpty()) text = oldContent + "\n" + text;
+        writeContents(storyFile,text);
+        System.out.println(readContentsAsString(storyFile));
     }
 
     /**
@@ -49,7 +61,9 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog aimDog = new Dog(name, breed, age);
+        aimDog.saveDog();
+        System.out.println(aimDog);
     }
 
     /**
@@ -59,6 +73,8 @@ public class CapersRepository {
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog aimDog = readObject(Utils.join(Dog.DOG_FOLDER,name), Dog.class);
+        aimDog.haveBirthday();
+        aimDog.saveDog();
     }
 }
--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,16 +1,19 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.NoSuchElementException;
+
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
  * @author TODO
 */
-public class Dog { // TODO
+public class Dog implements Serializable{ // TODO
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
+    static final File DOG_FOLDER = join(CapersRepository.CAPERS_FOLDER,"dogs"); // TODO (hint: look at the `join`
                                          //      function in Utils)
 
     /** Age of dog. */
@@ -39,7 +42,17 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
+        File aimFile = Utils.join(DOG_FOLDER,name);
+        if(!aimFile.exists()){
+            throw new NoSuchElementException("No such dog");
+        }
+        else{
+            try{
+                return Utils.readObject(aimFile,Dog.class);
+            } catch (Exception e){
+                Utils.exitWithError("read dog failure.");
+            }
+        }
         return null;
     }
 
@@ -56,7 +69,15 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File dogFile = Utils.join(DOG_FOLDER,this.name);
+        if(!dogFile.exists()){
+            try{
+                dogFile.createNewFile();
+            } catch (IOException e){
+                Utils.exitWithError("create dogFile failure");
+            }
+        }
+        writeObject(dogFile,this);
     }
 
     @Override
